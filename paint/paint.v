938252aacbe33e593cb183ac9c6cd97b1eef5198
NyW8 <tobluee@gmail.com>
2020-02-15 11:25:51 -0500

    Started quartus project

---
+++ paint/paint.v
@@ -0,0 +1,13 @@
+module paint(SW, KEY, LEDR, HEX0, HEX1, HEX2, HEX3);
+	input [17:0] SW;
+	input [4:0] KEY;
+	output [17:0] LEDR;
+	output [6:0] HEX0;
+	output [6:0] HEX1;
+	output [6:0] HEX2;
+	output [6:0] HEX3;
+	
+	/*
+	This is a top-level module, please don't add code here except for testing!
+	*/
+endmodule
